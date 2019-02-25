6f58800f7b660f70b0f61fe13c4af85f2a869131
jenda <jan.spidlen@gmail.com>
2019-02-24 19:58:23 -0800

    Update ReentrantDistributedLock.java

--- src/com/github/reentrantdistributedlock/ReentrantDistributedLock.java
+++ src/com/github/reentrantdistributedlock/ReentrantDistributedLock.java
@@ -17,10 +17,10 @@ import redis.clients.jedis.Transaction;
  * The implementation implements {@link Lock} so it can be used instead of
  * regular java locks.
  * 
- * The current implementation is guarded by expiring keys so it cannot never
- * deadlock forever in case of disappearing lock holder.
+ * The current implementation is guarded by expiring keys so it cannot
+ * deadlock forever in case of a disappearing lock holder.
  * 
- * If lock is to be held for a longer period of time, it's important to keep
+ * If tje lock is to be held for a longer period of time, it's important to keep
  * refreshing it using {@link #tryLock()} (or some other locking method) so the
  * key doesn't expire.
  * 
